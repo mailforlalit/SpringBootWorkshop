9363fec7ec60c47973243a0d8365c3e4019ee689
LALIT GUPTA <39664049+mailforlalit@users.noreply.github.com>
2026-02-22 00:43:16 +0530

    Test File change

---
+++ src/test/java/com/example/FebWorkshop/FebWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FebWorkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FebWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
